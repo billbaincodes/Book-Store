7b423e2972315dba839bc6048400eee2a6bd8e38
bill <williamandrewbain@gmail.com>
2018-12-04 15:13:12 -0700

    delete book route working

--- greads-front/src/components/Book.js
+++ greads-front/src/components/Book.js
@@ -2,16 +2,37 @@ import React from "react";
 import "../App.css";
 
 
-const Book = ({book}) => {
+const Book = ({ book, bookFetcher }) => {
+
+  const deleteBook = () => {
+
+    console.log(book.id)
+
+    fetch(`https://dry-atoll-97913.herokuapp.com/books/${book.id}`, {
+      method: "DELETE",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(bookFetcher)
+      .catch(console.log("error"))
+  }
+
+
     return (
+      
       <div className="book">
         <div className='book-info'>
           <h3>{book.title}</h3>
           <p>{book.genre}</p>
           <p>{book.description}</p>
+          <button onClick={deleteBook}>Delete book</button>
         </div>
         <img src={book.coverURL} alt="book cover"/>
       </div>
+
+
     );
 }
 
